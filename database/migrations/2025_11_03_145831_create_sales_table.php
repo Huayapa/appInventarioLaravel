1c3c82302eb158c11edb8585e43cfc2fa2d2d622
Huayapa <huayapajosue0@gmail.com>
2025-11-03 10:19:04 -0500

    models, migrations and controllers

--- database/migrations/2025_11_03_145831_create_sales_table.php
+++ database/migrations/2025_11_03_145831_create_sales_table.php
@@ -11,12 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('products', function (Blueprint $table) {
+        Schema::create('sales', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->text('description')->nullable();
-            $table->decimal('price', 10, 2);
-            $table->integer('stock')->default(0);
+            $table->foreignId('user_id')->nullable()->constrained('users')->nullOnDelete();
+            $table->dateTime('sale_date')->useCurrent();
+            $table->decimal('total', 10, 2);
             $table->timestamps();
         });
     }
@@ -26,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('products');
+        Schema::dropIfExists('sales');
     }
 };
