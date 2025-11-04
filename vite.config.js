26e0032eda060fe4f6327cdb7a83c8afd28182a6
josue Huayapa <huayapajosue0@gmail.com>
2025-11-03 21:14:58 -0500

    Agregar nuevos archivos CSS y actualizar la configuración de Vite y el manifiesto para incluir estilos de las páginas de creación, edición y visualización de productos y ventas.

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,27 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
+
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.css',
+                'resources/css/crear_productos.css',
+                'resources/css/create_detalle_ventas.css',
+                'resources/css/create_ventas.css',
+                'resources/css/dashboard.css',
+                'resources/css/edit_detalle_ventas.css',
+                'resources/css/edit_productos.css',
+                'resources/css/edit_ventas.css',
+                'resources/css/index_detalle_ventas.css',
+                'resources/css/index_ventas.css',
+                'resources/css/login.css',
+                'resources/css/productos.css',
+                'resources/css/register.css',
+                'resources/css/welcome.css',
+                'resources/js/app.js',
+            ],
             refresh: true,
         }),
     ],
